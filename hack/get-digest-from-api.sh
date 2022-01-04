ef6b7869a60cab4832acd6abebeb10e783493f53
Arstanaly Rysbekov <arstanrys@gmail.com>
2022-01-04 16:35:09 +0100

    Hack: start writing script to get docker image digest via API

---
+++ hack/get-digest-from-api.sh
@@ -0,0 +1,23 @@
+#https://medium.com/hackernoon/inspecting-docker-images-without-pulling-them-4de53d34a604
+get_token() {
+  local image=$1
+
+  echo "Retrieving Docker Hub token.
+    IMAGE: $image
+  " >&2
+
+  curl \
+    --silent \ 
+    --u "$DOCKER_USERNAME:$DOCKER_PASSWORD" \
+    "https://auth.docker.io/token?scope=repository:$image:pull&service=registry.docker.io" \
+    | jq -r '.token'
+}
+
+token=$(get_token merkely/change)
+
+curl \
+    --silent \
+    --header "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+    --header "Authorization: Bearer $token" \
+    "https://registry-1.docker.io/v2/merkely/change/manifests/latest" \
+    | jq -r '.config.digest'
