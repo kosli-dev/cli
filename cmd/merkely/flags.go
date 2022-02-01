b0dfa5a93ba529d493dbde811b0264ac815ac826
Sami Alajrami <sami@merkely.com>
2022-02-01 16:28:33 +0100

    update --registry=provider description

--- cmd/merkely/flags.go
+++ cmd/merkely/flags.go
@@ -11,7 +11,7 @@ type fingerprintOptions struct {
 
 func addFingerprintFlags(cmd *cobra.Command, o *fingerprintOptions) {
 	cmd.Flags().StringVarP(&o.artifactType, "artifact-type", "t", "", "The type of the artifact to calculate its SHA256 fingerprint.")
-	cmd.Flags().StringVar(&o.registryProvider, "registry-provider", "", "The docker registry provider. Allowed options are [dockerhub, github].")
+	cmd.Flags().StringVar(&o.registryProvider, "registry-provider", "", "The docker registry provider or url.")
 	cmd.Flags().StringVar(&o.registryUsername, "registry-username", "", "The docker registry username.")
 	cmd.Flags().StringVar(&o.registryPassword, "registry-password", "", "The docker registry password or access token.")
 }
