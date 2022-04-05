0b09025b2efdf2bc123ced639e5a5896eeb930a8
zhelezovartem <Zhelezovartem@gmail.com>
2022-04-05 18:39:25 +0400

    cleanup, execute ecs task every minute

--- deployment/terraform/iam.tf
+++ deployment/terraform/iam.tf
@@ -82,3 +82,61 @@ resource "aws_iam_role_policy_attachment" "task_ecs_list_allow" {
   role       = aws_iam_role.task.name
   policy_arn = aws_iam_policy.ecs_list_allow.arn
 }
+
+# EventBridge rule policy
+resource "aws_iam_role" "ecs_events" {
+  name = "ecs_events"
+
+  assume_role_policy = <<DOC
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "events.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+DOC
+}
+
+resource "aws_iam_role_policy" "ecs_events_run_task_with_any_role" {
+  name = "ecs_events_run_task_with_any_role"
+  role = aws_iam_role.ecs_events.id
+
+  policy = <<DOC
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "iam:PassRole",
+            "Resource": "*",
+            "Condition": {
+                "StringLike": {
+                    "iam:PassedToService": "ecs-tasks.amazonaws.com"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecs:RunTask"
+            ],
+            "Resource": [
+                "arn:aws:ecs:*:${data.aws_caller_identity.current.account_id}:task-definition/${var.app_name}:*"
+            ],
+            "Condition": {
+                "ArnLike": {
+                    "ecs:cluster": "arn:aws:ecs:*:${data.aws_caller_identity.current.account_id}:cluster/merkely"
+                }
+            }
+        }
+    ]
+}
+DOC
+}
