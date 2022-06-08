dc53dace11bb53320090aec944cda7b40c488e7d
Tore Martin Hagen <tore@merkely.com>
2022-06-08 14:56:30 +0200

    Environments: Added timestamps to ls -l command

--- cmd/merkely/environmentLs.go
+++ cmd/merkely/environmentLs.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 
 	"github.com/merkely-development/reporter/internal/requests"
 	"github.com/sirupsen/logrus"
@@ -55,11 +56,21 @@ func (o *environmentLsOptions) run(out io.Writer) error {
 		}
 
 		if o.long {
-			fmt.Printf("%-15s %s\n", "NAME", "TYPE")
+			fmt.Printf("%-15s %-10s %-27s %s\n", "NAME", "TYPE", "LAST REPORT", "LAST MODIFIED")
 		}
 		for _, env := range envs {
 			if o.long {
-				fmt.Printf("%-15s %s\n", env["name"], env["type"])
+				last_reported_str := ""
+				last_reported_at := env["last_reported_at"]
+				if last_reported_at != nil {
+					last_reported_str = time.Unix(int64(last_reported_at.(float64)), 0).Format(time.RFC3339)
+				}
+				last_modified_str := ""
+				last_modified_at := env["last_modified_at"]
+				if last_modified_at != nil {
+					last_modified_str = time.Unix(int64(last_modified_at.(float64)), 0).Format(time.RFC3339)
+				}
+				fmt.Printf("%-15s %-10s %-27s %s\n", env["name"], env["type"], last_reported_str, last_modified_str)
 			} else {
 				fmt.Println(env["name"])
 			}
