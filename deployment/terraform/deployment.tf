10673bea024245d6824d2a0ab3d31f01ae2b6443
zhelezovartem <Zhelezovartem@gmail.com>
2022-07-18 16:14:38 +0300

    tf fmt

--- deployment/terraform/deployment.tf
+++ deployment/terraform/deployment.tf
@@ -7,7 +7,7 @@ module "lambda_reporter" {
   for_each     = var.reporter_apps
   source       = "./lambda-reporter"
   name         = "${var.app_name}-${each.key}"
-  env    = var.env
+  env          = var.env
   kosli_host   = each.value.kosli_host
   REPORTER_TAG = var.REPORTER_TAG
 
