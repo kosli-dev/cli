9a3559117d8e1e8b6a328aefa1866b19482bca62
zhelezovartem <Zhelezovartem@gmail.com>
2022-08-16 16:48:44 +0300

    add job to upload staging reporter packages

--- bin/upload_reporter_package.sh
+++ bin/upload_reporter_package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -Eeu
 
 KOSLI_CLI_VERSION="${TAG:1}"
-curl -o merkely_$KOSLI_CLI_VERSION.tar.gz -L https://github.com/kosli-dev/cli/releases/download/v$KOSLI_CLI_VERSION/merkely_${KOSLI_CLI_VERSION}_linux_amd64.tar.gz
+curl -o merkely_$KOSLI_CLI_VERSION.tar.gz -L https://github.com/kosli-dev/cli/releases/download/v$KOSLI_CLI_VERSION/kosli_${KOSLI_CLI_VERSION}_linux_amd64.tar.gz
 tar -xf merkely_$KOSLI_CLI_VERSION.tar.gz
 chmod 755 deployment/reporter-lambda-src/*
 zip -j kosli_lambda_$KOSLI_CLI_VERSION.zip deployment/reporter-lambda-src/* kosli
