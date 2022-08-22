9f61d8fbcb6addb941e3b3b73d27100526dd3223
zhelezovartem <Zhelezovartem@gmail.com>
2022-08-22 13:43:59 +0300

    git diff

--- deployment/reporter-lambda-src/function.sh
+++ deployment/reporter-lambda-src/function.sh
@@ -1,5 +1,5 @@
 function handler () {
   EVENT_DATA=$1
   echo "$EVENT_DATA" 1>&2;
-  ./kosli environment report ecs $ENV -C $ECS_CLUSTER --owner $KOSLI_USER
+  ./kosli environment report ecs $KOSLI_ENV -C $ECS_CLUSTER --owner $KOSLI_ORG
 }
