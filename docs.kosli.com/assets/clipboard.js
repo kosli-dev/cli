6aae08bd40ab6533dd298fc16f4f01ccd6372684
Tore Martin Hagen <tore@merkely.com>
2022-08-24 09:01:48 +0200

    Cleaned up clipboard.js

--- docs.kosli.com/assets/clipboard.js
+++ docs.kosli.com/assets/clipboard.js
@@ -1,18 +1,6 @@
 (function () {
-  function select(element) {
-    const selection = window.getSelection();
-
-    const range = document.createRange();
-    range.selectNodeContents(element);
-
-    selection.removeAllRanges();
-    selection.addRange(range);
-  }
-
   document.querySelectorAll("div.command").forEach(code => {
     code.addEventListener("click", function (event) {
-      // select(code.parentElement);
-
       if (navigator.clipboard) {
         navigator.clipboard.writeText(code.textContent);
       }
