3e0af05addf83ae6438a0bfb2c2e4e1761f966d1
Tore Martin Hagen <tore@merkely.com>
2022-08-25 08:35:18 +0200

    Getting started: Added 'Copied' text when user has copied the text

--- docs.kosli.com/assets/clipboard.js
+++ docs.kosli.com/assets/clipboard.js
@@ -1,8 +1,17 @@
 (function () {
   document.querySelectorAll("div.command").forEach(code => {
+    let tag = document.createElement("div");
+    let text = document.createTextNode("Copied!")
+    tag.appendChild(text);
+    code.appendChild(tag);
+    tag.classList.add("copiedText");
     code.addEventListener("click", function (event) {
       if (navigator.clipboard) {
         navigator.clipboard.writeText(code.textContent);
+        tag.classList.add("visible");
+        setTimeout(() => {
+          tag.classList.remove("visible");        
+        }, 1500);
       }
     });
   });
