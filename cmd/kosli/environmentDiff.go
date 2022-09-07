50ee801dc95be3f44c45a20fcc64d3e9d90c3128
Sami Alajrami <sami@merkely.com>
2022-09-07 14:13:10 +0200

    Commands: update env diff output to match API updates

--- cmd/kosli/environmentDiff.go
+++ cmd/kosli/environmentDiff.go
@@ -19,6 +19,17 @@ type environmentDiffOptions struct {
 }
 
 type EnvironmentDiffResponse struct {
+	Snappish1 DiffSnappishItem `json:"snappish1"`
+	Snappish2 DiffSnappishItem `json:"snappish2"`
+	Changed   []DiffArtifact   `json:"0"`
+}
+
+type DiffSnappishItem struct {
+	SnapshotID string         `json:"snapshot_id"`
+	Artifacts  []DiffArtifact `json:"artifacts"`
+}
+
+type DiffArtifact struct {
 	Sha256              string   `json:"sha256"`
 	Pipeline            string   `json:"pipeline"`
 	Name                string   `json:"name"`
@@ -79,19 +90,26 @@ func (o *environmentDiffOptions) run(out io.Writer, args []string) error {
 }
 
 func printEnvironmentDiffAsTable(snappish1, snappish2, raw string, out io.Writer, page int) error {
-	var diffs map[string][]EnvironmentDiffResponse
+	var diffs EnvironmentDiffResponse
 	err := json.Unmarshal([]byte(raw), &diffs)
 	if err != nil {
 		return err
 	}
 
-	s1Count := len(diffs[snappish1])
-	s2Count := len(diffs[snappish2])
-	changedCount := len(diffs["0"])
+	s1Artifacts := diffs.Snappish1.Artifacts
+	s2Artifacts := diffs.Snappish2.Artifacts
+	changedArtifacts := diffs.Changed
+	s1Count := len(s1Artifacts)
+	s2Count := len(s2Artifacts)
+	changedCount := len(changedArtifacts)
 
 	if s1Count > 0 {
-		fmt.Printf("%s only\n", snappish1)
-		for _, entry := range diffs[snappish1] {
+		if snappish1 == diffs.Snappish1.SnapshotID {
+			fmt.Printf("only present in %s\n", snappish1)
+		} else {
+			fmt.Printf("only present in %s (snapshot: %s)\n", snappish1, diffs.Snappish1.SnapshotID)
+		}
+		for _, entry := range s1Artifacts {
 			err := printOnlyEntry(entry)
 			if err != nil {
 				return err
@@ -100,12 +118,16 @@ func printEnvironmentDiffAsTable(snappish1, snappish2, raw string, out io.Writer
 	}
 
 	if s1Count > 0 && s2Count > 0 {
-		fmt.Println()
+		fmt.Println("------------")
 	}
 
 	if s2Count > 0 {
-		fmt.Printf("%s only\n", snappish2)
-		for _, entry := range diffs[snappish2] {
+		if snappish2 == diffs.Snappish2.SnapshotID {
+			fmt.Printf("only present in %s\n", snappish2)
+		} else {
+			fmt.Printf("only present in %s (snapshot: %s)\n", snappish2, diffs.Snappish2.SnapshotID)
+		}
+		for _, entry := range s2Artifacts {
 			err := printOnlyEntry(entry)
 			if err != nil {
 				return err
@@ -114,25 +136,26 @@ func printEnvironmentDiffAsTable(snappish1, snappish2, raw string, out io.Writer
 	}
 
 	if changedCount > 0 && (s1Count > 0 || s2Count > 0) {
-		fmt.Println()
+		fmt.Println("------------")
 	}
 
 	if changedCount > 0 {
 		fmt.Printf("%s -> %s scaling\n", snappish1, snappish2)
-		for _, entry := range diffs["0"] {
+		for _, entry := range changedArtifacts {
 			err := printOnlyEntry(entry)
 			if err != nil {
 				return err
 			}
-			fmt.Printf("    Instances: scaled from %d to %d\n", entry.S1InstanceCount, entry.S2InstanceCount)
+			fmt.Printf("    Instances: scaled from %d to %d", entry.S1InstanceCount, entry.S2InstanceCount)
 		}
 	}
 
 	return nil
 }
 
-func printOnlyEntry(entry EnvironmentDiffResponse) error {
-	fmt.Printf("  Name: %s\n", entry.Name)
+func printOnlyEntry(entry DiffArtifact) error {
+	fmt.Println()
+	fmt.Printf("    Name: %s\n", entry.Name)
 
 	fmt.Printf("    Sha256: %s\n", entry.Sha256)
 
