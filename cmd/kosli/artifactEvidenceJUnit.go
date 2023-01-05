debc5f03e354171610b9bb9941077d53416adce7
Tore Martin Hagen <tore@kosli.com>
2023-01-05 14:53:57 +0100

    Evidence: Removed trailing slash from URL kosli-dev/server#571

--- cmd/kosli/artifactEvidenceJUnit.go
+++ cmd/kosli/artifactEvidenceJUnit.go
@@ -123,7 +123,7 @@ func (o *junitEvidenceOptions) run(args []string) error {
 	} else {
 		o.payload.ArtifactFingerprint = o.fingerprint
 	}
-	url := fmt.Sprintf("%s/api/v1/projects/%s/%s/evidence/junit/", global.Host, global.Owner, o.pipelineName)
+	url := fmt.Sprintf("%s/api/v1/projects/%s/%s/evidence/junit", global.Host, global.Owner, o.pipelineName)
 	o.payload.UserData, err = LoadJsonData(o.userDataFile)
 	if err != nil {
 		return err
