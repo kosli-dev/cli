cfff7d9d39e3d8f38500d7725f52fd30d3beb82b
Arstanaly Rysbekov <arstanrys@gmail.com>
2023-02-09 14:54:00 +0100

    Commands: add assert gitlab-mergerequest command

--- cmd/kosli/artifactEvidencePRGitlab_test.go
+++ cmd/kosli/artifactEvidencePRGitlab_test.go
@@ -147,32 +147,32 @@ func (suite *ArtifactEvidencePRGitlabCommandTestSuite) TestArtifactEvidencePRGit
 	runTestCmd(suite.T(), tests)
 }
 
-// func (suite *ArtifactEvidencePRGithubCommandTestSuite) TestAssertPRGitlabCmd() {
-// 	tests := []cmdTestCase{
-// 		{
-// 			name: "assert Github PR evidence passes when commit has a PR in github",
-// 			cmd: `assert github-pullrequest --github-org kosli-dev --repository cli
-// 			--commit 73d7fee2f31ade8e1a9c456c324255212c30c2a6` + suite.defaultKosliArguments,
-// 			golden: "found [1] pull request(s) in Github for commit: 73d7fee2f31ade8e1a9c456c324255212c30c2a6\n",
-// 		},
-// 		{
-// 			wantError: true,
-// 			name:      "assert Github PR evidence fails when commit has no PRs in github",
-// 			cmd: `assert github-pullrequest --github-org kosli-dev --repository cli
-// 			--commit 19aab7f063147614451c88969602a10afbabb43d` + suite.defaultKosliArguments,
-// 			golden: "Error: no pull requests found for the given commit: 19aab7f063147614451c88969602a10afbabb43d\n",
-// 		},
-// 		{
-// 			wantError: true,
-// 			name:      "assert Github PR evidence fails when commit does not exist",
-// 			cmd: `assert github-pullrequest --github-org kosli-dev --repository cli
-// 			--commit 19aab7f063147614451c88969602a10afba123ab` + suite.defaultKosliArguments,
-// 			golden: "Error: GET https://api.github.com/repos/kosli-dev/cli/commits/19aab7f063147614451c88969602a10afba123ab/pulls: 422 No commit found for SHA: 19aab7f063147614451c88969602a10afba123ab []\n",
-// 		},
-// 	}
+func (suite *ArtifactEvidencePRGitlabCommandTestSuite) TestAssertPRGitlabCmd() {
+	tests := []cmdTestCase{
+		{
+			name: "assert Gitlab PR evidence passes when commit has a PR in gitlab",
+			cmd: `assert gitlab-mergerequest --gitlab-org ewelinawilkosz --repository merkely-gitlab-demo
+			--commit e6510880aecdc05d79104d937e1adb572bd91911` + suite.defaultKosliArguments,
+			golden: "found [1] pull request(s) in Gitlab for commit: e6510880aecdc05d79104d937e1adb572bd91911\n",
+		},
+		{
+			wantError: true,
+			name:      "assert Gitlab PR evidence fails when commit has no PRs in gitlab",
+			cmd: `assert gitlab-mergerequest --gitlab-org ewelinawilkosz --repository merkely-gitlab-demo
+			--commit 2ec23dda01fc85e3f94a2b5ea8cb8cf7e79c4ed6` + suite.defaultKosliArguments,
+			golden: "Error: no merge requests found for the given commit: 2ec23dda01fc85e3f94a2b5ea8cb8cf7e79c4ed6\n",
+		},
+		{
+			wantError: true,
+			name:      "assert Gitlab PR evidence fails when commit does not exist",
+			cmd: `assert gitlab-mergerequest --gitlab-org kosli-dev --repository cli
+			--commit 1111111111111111111111111111111111111111` + suite.defaultKosliArguments,
+			golden: "Error: GET https://gitlab.com/api/v4/projects/kosli-dev/cli/repository/commits/1111111111111111111111111111111111111111/merge_requests: 404 {message: 404 Project Not Found}\n",
+		},
+	}
 
-// 	runTestCmd(suite.T(), tests)
-// }
+	runTestCmd(suite.T(), tests)
+}
 
 // In order for 'go test' to run this suite, we need to create
 // a normal test function and pass our suite to suite.Run
---
+++ cmd/kosli/assertPRGitlab.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"io"
+
+	"github.com/kosli-dev/cli/internal/gitlab"
+	"github.com/spf13/cobra"
+)
+
+const assertPRGitlabShortDesc = `Assert if a Gitlab pull request for a git commit exists.`
+
+const assertPRGitlabLongDesc = assertPRGitlabShortDesc + `
+The command exits with non-zero exit code 
+if no pull requests were found for the commit.`
+
+const assertPRGitlabExample = `
+kosli assert gitlab-mergerequest \
+	--github-token yourGithubToken \
+	--github-org yourGithubOrg \
+	--commit yourArtifactGitCommit \
+	--commit yourGitCommit \
+	--repository yourGithubGitRepository
+`
+
+func newAssertPullRequestGitlabCmd(out io.Writer) *cobra.Command {
+	o := new(pullRequestEvidenceGitlabOptions)
+	o.gitlabConfig = new(gitlab.GitlabConfig)
+	cmd := &cobra.Command{
+		Use:     "gitlab-mergerequest",
+		Short:   assertPRGitlabShortDesc,
+		Long:    assertPRGitlabLongDesc,
+		Example: assertPRGitlabExample,
+		Hidden:  true,
+		Args:    cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			o.assert = true
+			pullRequestsEvidence, err := o.getGitlabPullRequests()
+			if err != nil {
+				return err
+			}
+			logger.Info("found [%d] pull request(s) in Gitlab for commit: %s", len(pullRequestsEvidence), o.commit)
+			return nil
+		},
+	}
+
+	ci := WhichCI()
+	cmd.Flags().StringVar(&o.gitlabConfig.Token, "gitlab-token", "", gitlabTokenFlag)
+	cmd.Flags().StringVar(&o.gitlabConfig.Org, "gitlab-org", "", gitlabOrgFlag)
+	cmd.Flags().StringVar(&o.gitlabConfig.Repository, "repository", DefaultValue(ci, "repository"), repositoryFlag)
+	cmd.Flags().StringVar(&o.commit, "commit", DefaultValue(ci, "git-commit"), commitPREvidenceFlag)
+	addDryRunFlag(cmd)
+
+	err := RequireFlags(cmd, []string{
+		"gitlab-token", "gitlab-org", "commit", "repository",
+	})
+	if err != nil {
+		logger.Error("failed to configure required flags: %v", err)
+	}
+
+	return cmd
+}
