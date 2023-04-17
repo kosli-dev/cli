ece1356a6848952fb64d8c39cd34175c58f6eebd
Sami Alajrami <sami@kosli.com>
2023-04-17 13:57:37 +0200

    imporve error handling in reset-or-restart-server test setup script

--- bin/reset-or-start-server.sh
+++ bin/reset-or-start-server.sh
@@ -3,6 +3,16 @@
 force_restart=$1
 container_name=cli_kosli_server
 
+check_success()
+{
+	if [ $? -eq 0 ]; then
+    	echo -e "completed \xE2\x9C\x94"
+	else
+   		echo -e "failed \xE2\x9D\x8C"
+		exit 52
+	fi
+}
+
 restart_server() 
 {
 	echo restarting server ...
@@ -12,21 +22,21 @@ restart_server()
 	docker-compose up -d
 	./mongo/ip_wait.sh localhost:9010/minio/health/live
 	./mongo/ip_wait.sh localhost:8001/ready
-	echo -e "completed \xE2\x9C\x94"
+	check_success
 }
 
 
 if [ ! -z "$force_restart" ]; then
 	restart_server
-elif [ "$( docker container inspect -f '{{.State.Running}}' $container_name )" == "true" ]; then
+elif [ "$( docker container inspect -f '{{.State.Status}}' $container_name )" == "running" ]; then
     echo reseting DB on running server ...
-	docker exec $container_name /app/test/clean_database.py
-	echo -e "completed \xE2\x9C\x94"
+	docker exec $container_name /app/test/clean_database.py 
+	check_success
 else
 	restart_server
 fi
 
 echo creating test users on server ...
 docker exec $container_name /demo/create_test_users.py
-echo -e "completed \xE2\x9C\x94"
+check_success
 
