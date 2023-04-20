3d0ce58449eb1b29f9943ef930a5f6f25a122ae6
Arstanaly Rysbekov <arstanrys@gmail.com>
2023-04-20 13:14:46 +0200

    Tests: set server image to hardcoded version from a branch to simplify testing

--- bin/reset-or-start-server.sh
+++ bin/reset-or-start-server.sh
@@ -18,7 +18,8 @@ restart_server()
 	echo restarting server ...
 	./bin/docker_login_aws.sh staging
     docker-compose down || true
-	docker pull 772819027869.dkr.ecr.eu-central-1.amazonaws.com/merkely:latest || true
+	# TODO: set this to the latest version before merging to main
+	docker pull 772819027869.dkr.ecr.eu-central-1.amazonaws.com/merkely:aad4451 || true
 	docker-compose up -d
 	./mongo/ip_wait.sh localhost:9010/minio/health/live
 	./mongo/ip_wait.sh localhost:8001/ready
