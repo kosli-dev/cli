b1a4636b315a4f92258909c90c8f710d85c6d0fe
Arstanaly Rysbekov <arstanrys@gmail.com>
2023-04-20 13:23:27 +0200

    Tests: fix audit trail creation tests

--- cmd/kosli/createAuditTrail_test.go
+++ cmd/kosli/createAuditTrail_test.go
@@ -35,35 +35,35 @@ func (suite *CreateAuditTrailCommandTestSuite) TestCreateAuditTrailCmd() {
 		{
 			wantError: true,
 			name:      "fails when name is considered invalid by the server",
-			cmd:       "create audit-trail foo_bar" + suite.defaultKosliArguments,
+			cmd:       "create audit-trail foo_bar --steps step1,step2" + suite.defaultKosliArguments,
 			golden:    "Error: Input payload validation failed: map[name:'foo_bar' does not match '^[a-zA-Z0-9\\\\-]+$']\n",
 		},
 		{
 			name:   "can create an audit trail",
-			cmd:    "create audit-trail newAuditTrail --description \"my new audit trail\" " + suite.defaultKosliArguments,
-			golden: "flow 'newAuditTrail' was created\n",
+			cmd:    "create audit-trail newAuditTrail --description \"my new audit trail\" --steps step1,step2" + suite.defaultKosliArguments,
+			golden: "audit trail 'newAuditTrail' was created\n",
 		},
 		{
 			name:   "re-creating a flow updates its metadata",
-			cmd:    "create audit-trail newAuditTrail --description \"changed description\" " + suite.defaultKosliArguments,
+			cmd:    "create audit-trail newAuditTrail --description \"changed description\" --steps step1,step2" + suite.defaultKosliArguments,
 			golden: "audit trail 'newAuditTrail' was created\n",
 		},
 		{
 			wantError: true,
 			name:      "missing --org flag causes an error",
-			cmd:       "create audit-trail newAuditTrail --description \"my new audit trail\" -H http://localhost:8001 -a eyJhbGciOiJIUzUxMiIsImlhdCI6MTYyNTY0NDUwMCwiZXhwIjoxNjI1NjQ4MTAwfQ.eyJpZCI6IjgzYTBkY2Q1In0.1B-xDlajF46vipL49zPbnXBRgotqGGcB3lxwpJxZ3HNce07E0p2LwO7UDYve9j2G9fQtKrKhUKvVR97SQOEFLQ",
+			cmd:       "create audit-trail newAuditTrail --description \"my new audit trail\" --steps step1,step2 -H http://localhost:8001 -a eyJhbGciOiJIUzUxMiIsImlhdCI6MTYyNTY0NDUwMCwiZXhwIjoxNjI1NjQ4MTAwfQ.eyJpZCI6IjgzYTBkY2Q1In0.1B-xDlajF46vipL49zPbnXBRgotqGGcB3lxwpJxZ3HNce07E0p2LwO7UDYve9j2G9fQtKrKhUKvVR97SQOEFLQ",
 			golden:    "Error: --org is not set\nUsage: kosli create audit-trail [AUDIT-TRAIL-NAME] [flags]\n",
 		},
 		{
 			wantError: true,
 			name:      "missing --api-token flag causes an error",
-			cmd:       "create audit-trail newAuditTrail --description \"my new audit trail\" --org cyber-dojo -H http://localhost:8001",
+			cmd:       "create audit-trail newAuditTrail --description \"my new audit trail\" --steps step1,step2 --org cyber-dojo -H http://localhost:8001",
 			golden:    "Error: --api-token is not set\nUsage: kosli create audit-trail [AUDIT-TRAIL-NAME] [flags]\n",
 		},
 		{
 			wantError: true,
 			name:      "missing name argument fails",
-			cmd:       "create audit-trail --description \"my new flow\" -H http://localhost:8001 --org cyber-dojo -a eyJhbGciOiJIUzUxMiIsImlhdCI6MTYyNTY0NDUwMCwiZXhwIjoxNjI1NjQ4MTAwfQ.eyJpZCI6IjgzYTBkY2Q1In0.1B-xDlajF46vipL49zPbnXBRgotqGGcB3lxwpJxZ3HNce07E0p2LwO7UDYve9j2G9fQtKrKhUKvVR97SQOEFLQ",
+			cmd:       "create audit-trail --description \"my new flow\" --steps step1,step2" + suite.defaultKosliArguments,
 			golden:    "Error: audit trail name must be provided as an argument\n",
 		},
 	}
