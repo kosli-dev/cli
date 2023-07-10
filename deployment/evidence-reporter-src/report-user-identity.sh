21cdbfbd63640d4b2835c6387adb363a46f2f678
zhelezovartem <Zhelezovartem@gmail.com>
2023-07-10 19:18:03 +0600

    cleanup

--- deployment/evidence-reporter-src/report-user-identity.sh
+++ deployment/evidence-reporter-src/report-user-identity.sh
@@ -14,8 +14,8 @@ function handler () {
   fi
 
   # Get ECS exec session user identity (ARN of the IAM role that initiated the session)
-  ECS_EXEC_USER_IDENTITY=$(echo ${EVENT_DATA} | jq -r ".detail.userIdentity.arn")
-  echo "{\"ecs_exec_role_arn\": \"${ECS_EXEC_USER_IDENTITY}\"}" | jq . > /tmp/user-identity.json
+  ECS_EXEC_USER_IDENTITY=$(echo ${EVENT_DATA} | jq ".detail.userIdentity.arn")
+  echo "{\"ecs_exec_role_arn\": ${ECS_EXEC_USER_IDENTITY}}" | jq . > /tmp/user-identity.json
 
   echo "Reporting ECS exec user identity to the Kosli..." 1>&2
   ./kosli report evidence workflow --audit-trail ${KOSLI_AUDIT_TRAIL_NAME} \
@@ -25,15 +25,9 @@ function handler () {
       --step ${KOSLI_STEP_NAME_USER_IDENTITY}
 
   # Get ECS exec session service identity
-  echo "Getting ECS task ARN..." 1>&2
   ECS_EXEC_TASK_ARN=$(echo ${EVENT_DATA} | jq -r ".detail.responseElements.taskArn")
-  echo "ECS task ARN is ${ECS_EXEC_TASK_ARN}" 1>&2
-  echo "Getting ECS Cluster name..." 1>&2
   ECS_EXEC_CLUSTER=$(echo ${EVENT_DATA} | jq -r ".detail.requestParameters.cluster")
-  echo "ECS Cluster name is ${ECS_EXEC_CLUSTER}" 1>&2
-  echo "Getting ECS task group..." 1>&2
   ECS_EXEC_TASK_GROUP=$(aws ecs describe-tasks --cluster ${ECS_EXEC_CLUSTER} --tasks ${ECS_EXEC_TASK_ARN} | jq ".tasks[].group")
-  echo "ECS task group is ${ECS_EXEC_TASK_GROUP}" 1>&2
 
   echo "{\"ecs_exec_service_identity\": ${ECS_EXEC_TASK_GROUP}}" | jq . > /tmp/service-identity.json
 
