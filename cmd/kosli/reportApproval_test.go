40df48037cb879c8853c5e7511dbbd7a44fae411
Simon Castagna <sd.castagna@gmail.com>
2023-11-27 12:03:29 +0100

    Create setup to make a snapshot in approval tests

--- cmd/kosli/reportApproval_test.go
+++ cmd/kosli/reportApproval_test.go
@@ -14,6 +14,11 @@ type ApprovalReportTestSuite struct {
 	flowName              string
 	envName               string
 	gitCommit             string
+	artifactPath          string
+}
+
+type reportApprovalTestConfig struct {
+	createSnapshot bool
 }
 
 func (suite *ApprovalReportTestSuite) SetupTest() {
@@ -24,15 +29,16 @@ func (suite *ApprovalReportTestSuite) SetupTest() {
 	}
 
 	suite.defaultKosliArguments = fmt.Sprintf(" --host %s --org %s --api-token %s", global.Host, global.Org, global.ApiToken)
-	suite.artifactFingerprint = "847411c6124e719a4e8da2550ac5c116b7ff930493ce8a061486b48db8a5aaa0"
 	suite.flowName = "approval-test"
 	suite.envName = "staging"
 	suite.gitCommit = "993a9a6be532ed4e7a87aab4df90a7f1b3168d63"
+	suite.artifactPath = "testdata/report.xml"
+	suite.artifactFingerprint = "c6b02fb1708fbc46e63f5074c38d17852bfa0c7bcfcdd1f877e80476e3fcb74f"
+	// _, suite.artifactFingerprint, _ = executeCommandC(fmt.Sprintf("kosli fingerprint" + suite.artifactPath + "--artifact-type file" + suite.defaultKosliArguments))
 
 	CreateFlow(suite.flowName, suite.T())
-	CreateArtifactWithCommit(suite.flowName, suite.artifactFingerprint, "foobar", suite.gitCommit, suite.T())
-	CreateEnv(global.Org, suite.envName, "K8S", suite.T())
-
+	CreateArtifactWithCommit(suite.flowName, suite.artifactFingerprint, suite.artifactPath, suite.gitCommit, suite.T())
+	CreateEnv(global.Org, suite.envName, "server", suite.T())
 }
 
 func (suite *ApprovalReportTestSuite) TestApprovalReportCmd() {
@@ -57,23 +63,32 @@ func (suite *ApprovalReportTestSuite) TestApprovalReportCmd() {
 			golden: "Error: at least one of --environment, --oldest-commit is required\n",
 		},
 		// For the next test we have to
-		// - (create a flow)
+		// - create a flow
 		// - create an artifact in that flow with git commit HEAD~5
 		// - (create an environment)
 		// - create snapshot that contains this artifact
-		// {
-		// 	name: "report approval with an environment name and no oldest-commit and no newest-commit works",
-		// 	cmd: `report approval --fingerprint ` + suite.artifactFingerprint + ` --flow ` + suite.flowName + ` --repo-root ../.. ` +
-		// 		` --environment ` + suite.envName + suite.defaultKosliArguments,
-		// 	golden: fmt.Sprintf("approval created for artifact: %s\n", suite.artifactFingerprint),
-		// },
+		{
+			name: "report approval with an environment name and no oldest-commit and no newest-commit works",
+			cmd: `report approval --fingerprint ` + suite.artifactFingerprint + ` --flow ` + suite.flowName + ` --repo-root ../.. ` +
+				` --environment ` + suite.envName + suite.defaultKosliArguments,
+			golden: fmt.Sprintf("approval created for artifact: %s\n", suite.artifactFingerprint),
+			additionalConfig: reportApprovalTestConfig{
+				createSnapshot: true,
+			},
+		},
 
 		// Here is a case we need to investigate how to test:
 		// - Create approval with '--newest-commit HEAD~5', '--oldest-commit HEAD~7' and '--environment staging',
 		//   then create approval only with '--environment staging',
 		// 	 the resulting payload should contain a commit list of 4 commits, and an oldest_commit of HEAD~5
 	}
-	runTestCmd(suite.T(), tests)
+	for _, t := range tests {
+		if t.additionalConfig != nil && t.additionalConfig.(reportApprovalTestConfig).createSnapshot {
+			ReportServerArtifactToEnv([]string{suite.artifactPath}, suite.envName, suite.T())
+		}
+		runTestCmd(suite.T(), []cmdTestCase{t})
+	}
+	// runTestCmd(suite.T(), tests)
 }
 
 func TestApprovalReportCommandTestSuite(t *testing.T) {
