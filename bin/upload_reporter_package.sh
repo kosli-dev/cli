15feab00988e54d71c90ca6fc8ac0a337871f41b
Artem Zhelezov <36639304+zhelezovartem@users.noreply.github.com>
2024-02-06 18:53:05 +0600

    Move env reporter src files to the terraform-aws-kosli-reporter repo (#114)
    * move environment-reporter src files and ci job to upload lambda packages to the separate repo
    
    * cleanup

--- bin/upload_reporter_package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -Eeu
-
-KOSLI_CLI_VERSION=$TAG
-make build
-chmod 755 deployment/reporter-lambda-src/*
-zip -j kosli_lambda_$KOSLI_CLI_VERSION.zip deployment/reporter-lambda-src/* kosli
-aws s3 cp kosli_lambda_$KOSLI_CLI_VERSION.zip s3://$S3_NAME/kosli_lambda_$KOSLI_CLI_VERSION.zip
