34505663721a0875749b4fe336631975e43093d5
Sami Alajrami <sami@kosli.com>
2024-02-21 14:31:33 +0100

    allow glob patterns in server snapshot paths

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -34,53 +34,52 @@ func (suite *ServerTestSuite) AfterTest() {
 	require.NoErrorf(suite.T(), err, "error cleaning up the temporary test directory %s", suite.tmpDir)
 }
 
+type fileSystemEntry struct {
+	files      map[string]string          // file names in a dir and their content
+	nestedDirs map[string]fileSystemEntry // nested dirs in a dir
+}
+
 func (suite *ServerTestSuite) TestCreateServerArtifactsData() {
-	type fileSystemEntry struct {
-		name     string
-		content  string            // file content (if entry is a file)
-		children map[string]string // dir files (if entry is dir)
-	}
 
 	for _, t := range []struct {
-		name       string
-		fileSystem map[string][]fileSystemEntry
-		want       []map[string]string
+		name         string
+		fileSystem   map[string]fileSystemEntry
+		paths        []string
+		excludePaths []string
+		want         []map[string]string
 	}{
 		{
 			name: "can get artifact data for a single path",
-			fileSystem: map[string][]fileSystemEntry{
+			fileSystem: map[string]fileSystemEntry{
 				"directory-name": {
-					{
-						name:     "sample.txt",
-						content:  "some content.",
-						children: make(map[string]string),
+					files: map[string]string{
+						"sample.txt": "some content.",
 					},
+					nestedDirs: make(map[string]fileSystemEntry),
 				},
 			},
-
+			paths: []string{"directory-name"},
 			want: []map[string]string{
 				{"directory-name": "388ab80164bbd9d96f132b046b8d09354f68b79a3668da7b507625cd1230dddf"},
 			},
 		},
 		{
 			name: "can get artifact data for two paths",
-			fileSystem: map[string][]fileSystemEntry{
+			fileSystem: map[string]fileSystemEntry{
 				"directory-name2": {
-					{
-						name:     "sample.txt",
-						content:  "some content.",
-						children: make(map[string]string),
+					files: map[string]string{
+						"sample.txt": "some content.",
 					},
+					nestedDirs: make(map[string]fileSystemEntry),
 				},
 				"directory-name3": {
-					{
-						name:     "sample-2.txt",
-						content:  "some more content.",
-						children: make(map[string]string),
+					files: map[string]string{
+						"sample-2.txt": "some more content.",
 					},
+					nestedDirs: make(map[string]fileSystemEntry),
 				},
 			},
-
+			paths: []string{"directory-name2", "directory-name3"},
 			want: []map[string]string{
 				{"directory-name2": "388ab80164bbd9d96f132b046b8d09354f68b79a3668da7b507625cd1230dddf"},
 				{"directory-name3": "3025bae51416f4348cbeaf3d2f7394a21d637792c850fb63d6c5242f073bc9b3"},
@@ -88,45 +87,91 @@ func (suite *ServerTestSuite) TestCreateServerArtifactsData() {
 		},
 		{
 			name: "can get digest of a directory containing a file with a space in its name",
-			fileSystem: map[string][]fileSystemEntry{
+			fileSystem: map[string]fileSystemEntry{
 				"directory-name4": {
-					{
-						name:     "SPV STS Prod.pem",
-						content:  "content",
-						children: make(map[string]string),
+					files: map[string]string{
+						"SPV STS Prod.pem": "content",
 					},
+					nestedDirs: make(map[string]fileSystemEntry),
 				},
 			},
-
+			paths: []string{"directory-name4"},
 			want: []map[string]string{
 				{"directory-name4": "0cccd704109ea889ed18739f9e0ed610b7993512a23998a6491886a9de77845d"},
 			},
 		},
+		{
+			name: "can get data with excludePaths that excludes a file",
+			fileSystem: map[string]fileSystemEntry{
+				"top-dir": {
+					files: map[string]string{
+						"file1.txt": "some content.",
+						"app.log":   "some logs here.",
+					},
+					nestedDirs: make(map[string]fileSystemEntry),
+				},
+			},
+			paths:        []string{"top-dir"},
+			excludePaths: []string{"*.log"},
+			want: []map[string]string{
+				{"top-dir": "6b8174228c507be8c8d8482c2516b9c4775e401810db649b0cab40e75104f3a0"},
+			},
+		},
+		{
+			name: "can get data with paths containing a Glob pattern",
+			fileSystem: map[string]fileSystemEntry{
+				"glob-dir1": { // should be included
+					files: map[string]string{},
+					nestedDirs: map[string]fileSystemEntry{
+						"nested-glob-1": {
+							files: map[string]string{
+								"file1": "new content",
+							},
+							nestedDirs: map[string]fileSystemEntry{},
+						},
+					},
+				},
+				"glob-dir2": { // should be included
+					files: map[string]string{
+						"file2.exe": "this is an executable.",
+						"app.log":   "some logs here.", // should be excluded
+					},
+					nestedDirs: make(map[string]fileSystemEntry),
+				},
+				"non-glob-dir": {
+					files: map[string]string{
+						"file1.custom": "some content.", // should be included as a separate artifact
+						"app.log":      "some logs here.",
+					},
+					nestedDirs: make(map[string]fileSystemEntry),
+				},
+			},
+			paths:        []string{"glob-dir*", "*/*.custom"},
+			excludePaths: []string{"*.log"},
+			want: []map[string]string{
+				{
+					"glob-dir1": "3efd00f4aa9088a0c07629b308bb77e338fc229ab032711f8202cb76f6d2e8e9",
+				},
+				{
+					"glob-dir2": "94839bc10d01803d9ebbacd5ab9edc470176fe233ea1f13861a54f604066c47d",
+				},
+				{
+					"non-glob-dir/file1.custom": "593cfe761544e1363f7594b403c222a4d93cc3f15246ba88d1efc3cb8e817cc5",
+				},
+			},
+		},
 	} {
 		suite.Run(t.name, func() {
-			paths := []string{}
-			for dirName, dirContent := range t.fileSystem {
-				dirPath := filepath.Join(suite.tmpDir, dirName)
-				err := os.Mkdir(dirPath, 0777)
-				require.NoErrorf(suite.T(), err, "error creating test dir %s", dirName)
-				paths = append(paths, dirPath)
-
-				for _, entry := range dirContent {
-					path := filepath.Join(suite.tmpDir, dirName, entry.name)
-					if len(entry.children) == 0 { // file
-						suite.createFileWithContent(path, entry.content)
-					} else { // dir
-						err := os.Mkdir(path, 0777)
-						require.NoErrorf(suite.T(), err, "error creating test dir %s", path)
-						for name, data := range entry.children {
-							filePath := filepath.Join(path, name)
-							suite.createFileWithContent(filePath, data)
-						}
-					}
-				}
+			suite.setupTestFileSystem(suite.tmpDir, t.fileSystem)
+			if t.excludePaths == nil {
+				t.excludePaths = []string{}
 			}
 
-			serverData, err := CreateServerArtifactsData(paths, []string{}, logger.NewStandardLogger())
+			for i, path := range t.paths {
+				t.paths[i] = filepath.Join(suite.tmpDir, path)
+			}
+
+			serverData, err := CreateServerArtifactsData(t.paths, t.excludePaths, logger.NewStandardLogger())
 			require.NoErrorf(suite.T(), err, "error creating server artifact data: %v", err)
 
 			digestsList := []map[string]string{}
@@ -147,6 +192,19 @@ func (suite *ServerTestSuite) TestCreateServerArtifactsData() {
 			assert.ElementsMatch(suite.T(), expected, digestsList, fmt.Sprintf("TestCreateServerArtifactsData: %s , got: %v -- want: %v", t.name, digestsList, expected))
 
 		})
+	}
+}
+
+func (suite *ServerTestSuite) setupTestFileSystem(tmpDir string, fs map[string]fileSystemEntry) {
+	for entryName, fsEntry := range fs {
+		dirPath := filepath.Join(tmpDir, entryName)
+		err := os.Mkdir(dirPath, 0777)
+		require.NoErrorf(suite.T(), err, "error creating test dir %s", entryName)
+
+		for fileName, fileContent := range fsEntry.files {
+			suite.createFileWithContent(filepath.Join(dirPath, fileName), fileContent)
+		}
+		suite.setupTestFileSystem(dirPath, fsEntry.nestedDirs)
 	}
 }
 
