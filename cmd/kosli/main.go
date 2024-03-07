5181e4ed79fd9842354fbc874736f51bd6dbe5ef
JonJagger <jon@kosli.com>
2024-03-07 20:03:03 +0000

    Start adding code for doubled hostname

--- cmd/kosli/main.go
+++ cmd/kosli/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 
 	log "github.com/kosli-dev/cli/internal/logger"
@@ -20,7 +21,63 @@ func init() {
 	kosliClient = requests.NewStandardKosliClient()
 }
 
+func DoubleHostCyberDojoCallArgs() bool {
+	cmd, err := newRootCmd(logger.Out, os.Args[1:])
+	if err == nil {
+		host := cmd.Flag("host")
+		apiToken := cmd.Flag("api-token")
+		org := cmd.Flag("org")
+		logger.Info("Host: %v", host)
+		logger.Info("ApiToken: %v", apiToken)
+		logger.Info("Org: %v", org)
+		/*if host.Value.String() == "https://app.kosli.com,https://staging.app.kosli.com" {
+			logger.Info("Host is Doubled")
+		}*/
+		return true
+	}
+	return false
+}
+
+func DoubleHostCyberDojoCalls() int {
+
+	// cmd, err := newRootCmd(logger.Out, os.Args[1:])
+	// TODO: Get real host/api-token values
+
+	// Hard-wire the two host/api-token values
+	// Run exec.command() twice
+	//     add --host=... and --api-token=...
+	//     add --debug --dry-run
+	//     capture output and err and handle as per Issue
+
+	prodArgs := append(os.Args[1:], "--dry-run", "--debug")
+	logger.Info("prodCmd: %s", os.Args[0])
+	logger.Info("prodArgs: %v", prodArgs)
+	prodCmd := exec.Command(os.Args[0], prodArgs...)
+	prodOutput, prodError := prodCmd.Output()
+	// TODO: get to here by preventing infinite loop!
+	logger.Info("prodOutput %s\n", prodOutput)
+
+	stagingArgs := append(os.Args[1:], "--dry-run", "--debug")
+	logger.Info("stagingCmd: %s", os.Args[0])
+	logger.Info("stagingArgs: %v", stagingArgs)
+	stagingCmd := exec.Command(os.Args[0], stagingArgs...)
+	stagingOutput, stagingError := stagingCmd.Output()
+	if stagingError != nil {
+		fmt.Printf("%s", stagingOutput)
+	}
+
+	status := 0
+	if prodError != nil && stagingError != nil {
+		status = 42
+	}
+	return status
+}
+
 func main() {
+	if DoubleHostCyberDojoCallArgs() {
+		status := DoubleHostCyberDojoCalls()
+		os.Exit(status)
+	}
 	cmd, err := newRootCmd(logger.Out, os.Args[1:])
 	if err != nil {
 		logger.Error(err.Error())
