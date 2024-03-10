14df7996aed21e245d5170c67244f5d7fd4c107d
JonJagger <jon@kosli.com>
2024-03-10 08:34:40 +0000

    Tidy comments [ci skip]

--- cmd/kosli/cyberDojo.go
+++ cmd/kosli/cyberDojo.go
@@ -5,12 +5,12 @@ Kosli uses CI pipelines in the cyber-dojo Org repos [*] for two purposes:
 1. public facing documentation
 2. private development purposes
 
-All Kosli CLI calls in [*] are made to _two_ servers
+All Kosli CLI calls in [*] are made to _two_ servers (because of 2)
   - https://app.kosli.com
-  - https://staging.app.kolsi.com  (because of 2)
+  - https://staging.app.kosli.com
 
-Explicitly making each Kosli CLI call in [*] twice is not an option because of 1)
-The least-worst option is to allow KOLSI_HOST and KOSLI_API_TOKEN to specify two values.
+Explicitly making each Kosli CLI call in [*] twice is not an option (because of 1)
+The least-worst option is to allow KOSLI_HOST and KOSLI_API_TOKEN to specify two values.
 */
 
 import (
@@ -28,9 +28,11 @@ const prodHostURL = "https://app.kosli.com"
 const stagingHostURL = "https://staging.app.kosli.com"
 
 func CyberDojoProdAndStagingCallArgs(args []string) ([]string, []string) {
-	// Returns two []string args, ready for two calls to "inner_main"
-	//  - one to https://app.kosli.com
-	//  - one to https://staging.app.kosli.com
+	// If the args call is a double-host, double-api-token cyber-dojo call then
+	// return two []string args, modified so a call with those args targets two hosts:
+	//  - https://app.kosli.com
+	//  - https://staging.app.kosli.com
+	// Otherwise return nil, nil to indicate this is not a doubled-call.
 
 	orgs := splitGlobal(args, getOrg)
 	hosts := splitGlobal(args, getHost)
@@ -52,8 +54,8 @@ func CyberDojoProdAndStagingCallArgs(args []string) ([]string, []string) {
 
 		argsProd := argsAppendHostApiToken(0)
 		argsStaging := argsAppendHostApiToken(1)
-		//fmt.Printf("argsProd == <%s>\n", strings.Join(argsProd, " "))
-		//fmt.Printf("argsStaging == <%s>\n", strings.Join(argsStaging, " "))
+		// fmt.Printf("argsProd == %s\n", strings.Join(argsProd, " "))
+		// fmt.Printf("argsStaging == %s\n", strings.Join(argsStaging, " "))
 		return argsProd, argsStaging
 	} else {
 		return nil, nil
@@ -61,7 +63,9 @@ func CyberDojoProdAndStagingCallArgs(args []string) ([]string, []string) {
 }
 
 func CyberDojoRunProdAndStagingCalls(prodArgs []string, stagingArgs []string) error {
-	// Calls "inner_main" twice, once with prodArgs, once with stagingArgs
+	// Calls "inner_main" twice:
+	//  - with prodArgs targetting https://app.kosli.com
+	//  - with stagingArgs targetting https://staging.app.kosli.com
 	// If the prod-call and the staging-call succeed:
 	// 	- do NOT print the staging-call output, so it looks as-if only the prod call occurred.
 	// If the staging-call fails:
@@ -71,7 +75,6 @@ func CyberDojoRunProdAndStagingCalls(prodArgs []string, stagingArgs []string) er
 	prodOutput, prodErr := runBufferedInnerMain(prodArgs)
 	fmt.Print(prodOutput)
 	_, stagingErr := runBufferedInnerMain(stagingArgs)
-	// TODO?: print stagingOutput if --debug
 
 	var errorMessage string
 	if prodErr != nil {
@@ -89,17 +92,17 @@ func CyberDojoRunProdAndStagingCalls(prodArgs []string, stagingArgs []string) er
 }
 
 func runBufferedInnerMain(args []string) (string, error) {
-	// There is a logger.Error(..) call in main. The non-buffered global logger
-	// must be restored so the error messages actually appear.
+	// There is a logger.Error(..) call in main. It must be restored to use
+	// the non-buffered global logger so the error messages actually appear.
 	globalLogger := &logger
 	defer func(logger *log.Logger) { *globalLogger = logger }(logger)
-	// Use a buffered Writer so the output of the staging call is NOT printed
+	// Use a buffered Writer so output printing is decided by the caller.
 	var buffer bytes.Buffer
 	writer := io.Writer(&buffer)
 	// Set global logger
 	logger = log.NewLogger(writer, writer, false)
 	// We have to reset os.Args here.
-	// Presumably viper is reading os.Args?
+	// Presumably because viper is reading os.Args.
 	// Note that newRootCmd(args) does not use its args parameter.
 	os.Args = args
 	// Ensure prod/staging calls do not interfere with each other.
@@ -113,12 +116,14 @@ type getter func() string
 
 func splitGlobal(args []string, g getter) []string {
 	defer resetGlobal()
+	// Ignore any error, we want only to set the global fields.
 	_ = nullCmd(args).Execute()
+	// Note: cyber-dojo must ensure its api-tokens do not contain commas.
 	return strings.Split(g(), ",")
 }
 
 func resetGlobal() {
-	global = &GlobalOpts{}
+	global = new(GlobalOpts)
 }
 
 func nullCmd(args []string) *cobra.Command {
@@ -131,9 +136,11 @@ func nullCmd(args []string) *cobra.Command {
 func getOrg() string {
 	return global.Org
 }
+
 func getHost() string {
 	return global.Host
 }
+
 func getApiToken() string {
 	return global.ApiToken
 }
