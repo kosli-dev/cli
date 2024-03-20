5ca5d4cc533f73cdd355e10ba6f3202dac7016b9
Sami Alajrami <sami@kosli.com>
2024-03-20 11:58:22 +0100

    add hidden config command

---
+++ cmd/kosli/config.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/kosli-dev/cli/internal/security"
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/zalando/go-keyring"
+)
+
+type configOptions struct {
+	setKeys   map[string]string
+	unSetKeys []string
+}
+
+const configShortDesc = `Config global Kosli flags values and store them in $HOME/.kosli .  `
+
+const configLongDesc = configShortDesc + `
+
+Flag values are determined in the following order (highest precedence first):
+- command line flags on each executed command.
+- environment variables.
+- custom config file provided with --config-file flag.
+- default config file in $HOME/.kosli
+
+You can configure global Kosli flags (the ones that apply to all/most commands) using their dedicated
+convenience flags (e.g. --org). 
+
+API tokens are stored in the suitable credentials manager on your machine. 
+
+Other Kosli flags can be configured using the --set flag which takes a comma-separated list of key=value pairs.
+Keys correspond to the specific flag name, capitalized. For instance: --flow would be set using --set FLOW=value
+`
+
+const configExample = `
+# configure global flags in your default config file
+kosli config --org=yourOrg \
+	--api-token=yourAPIToken \
+	--host=https://app.kosli.com \
+	--debug=false \
+	--max-api-retries=3
+
+# configure non-global flags in your default config file
+kosli config --set FLOW=yourFlowName
+
+# remove a key from the default config file
+kosli config --unset FLOW
+`
+
+func newConfigCmd(out io.Writer) *cobra.Command {
+	o := new(configOptions)
+	cmd := &cobra.Command{
+		Use:     "config",
+		Short:   configShortDesc,
+		Long:    configLongDesc,
+		Example: configExample,
+		Hidden:  true,
+		Args:    cobra.NoArgs,
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if global.ConfigFile != defaultConfigFilePath() {
+				return fmt.Errorf("cannot use --config-file with config command")
+			}
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return o.run()
+		},
+	}
+
+	cmd.Flags().StringToStringVar(&o.setKeys, "set", map[string]string{}, setTagsFlag)
+	cmd.Flags().StringSliceVar(&o.unSetKeys, "unset", []string{}, unsetTagsFlag)
+
+	return cmd
+}
+
+func (o *configOptions) run() error {
+	home, err := homedir.Dir()
+	if err != nil {
+		return fmt.Errorf("setting default config failed. Failed to find home directory: %s", err)
+	}
+
+	path := filepath.Join(home, defaultConfigFilename)
+	permissions := os.FileMode(0600)
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		file, err := os.Create(path)
+		if err != nil {
+			return fmt.Errorf("setting default config failed. Error creating file: %s", err)
+		}
+		defer file.Close()
+
+		if err := file.Chmod(permissions); err != nil {
+			return fmt.Errorf("setting default config failed. Error setting file permissions: %s", err)
+		}
+
+		logger.Debug("default config file created successfully with permissions: %s", permissions)
+	} else if err != nil {
+		return fmt.Errorf("setting default config failed. Error checking file status: %s", err)
+	}
+
+	viper.SetConfigName(defaultConfigFilename)
+	viper.AddConfigPath(home)
+	viper.SetConfigType("yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return fmt.Errorf("setting default config failed. Error reading config file: %s", err)
+	}
+
+	if global.Org != "" {
+		viper.Set("org", global.Org)
+	}
+	if global.Host != defaultHost {
+		viper.Set("host", global.Host)
+	}
+	if global.ApiToken != "" {
+		// get encryption key
+		key, err := security.GetSecretFromCredentialsStore(credentialsStoreKeySecretName)
+		if err == keyring.ErrNotFound {
+			// create and save key
+			keyBytes, err := security.GenerateRandomAESKey()
+			if err != nil {
+				return err
+			}
+			key = string(keyBytes)
+			err = security.SetSecretInCredentialsStore(credentialsStoreKeySecretName, key)
+			if err != nil {
+				return err
+			}
+		} else if err != nil {
+			return err
+		}
+		if err != nil {
+			return err
+		}
+		// encrypt the token
+		encryptedTokenBytes, err := security.AESEncrypt(global.ApiToken, []byte(key))
+		if err != nil {
+			return err
+		}
+		viper.Set("api-token", string(encryptedTokenBytes))
+	}
+	if global.MaxAPIRetries != defaultMaxAPIRetries {
+		viper.Set("max-api-retries", global.MaxAPIRetries)
+	}
+
+	viper.Set("debug", global.Debug)
+	viper.Set("dry-run", global.DryRun)
+
+	for key, value := range o.setKeys {
+		viper.Set(key, value)
+	}
+
+	for _, key := range o.unSetKeys {
+		viper.Set(key, nil)
+	}
+
+	if err := viper.WriteConfig(); err != nil {
+		return fmt.Errorf("setting default config failed. Error writing config file: %s", err)
+	}
+
+	logger.Info("default config file updated successfully.")
+	return nil
+}
--- cmd/kosli/root.go
+++ cmd/kosli/root.go
@@ -7,6 +7,8 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/kosli-dev/cli/internal/security"
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
@@ -22,13 +24,19 @@ Setting the API token to DRY_RUN sets the --dry-run flag.
 `
 
 const (
-	maxAPIRetries = 3
+	defaultMaxAPIRetries = 3
 	// The name of our config file, without the file extension because viper supports many different config file languages.
-	defaultConfigFilename = "kosli"
+	defaultConfigFilename = ".kosli.yml"
+
+	// The default Kosli app host URL.
+	defaultHost = "https://app.kosli.com"
 
 	// The environment variable prefix of all environment variables bound to our command line flags.
 	// For example, --namespace is bound to KOSLI_NAMESPACE.
 	envPrefix = "KOSLI"
+
+	// the name of the credentials store secret holding the encryption key for api token storage
+	credentialsStoreKeySecretName = "kosli-encryption-key"
 
 	// the following constants are used in the docs/help
 	fingerprintDesc = "The artifact SHA256 fingerprint is calculated (based on --artifact-type flag) or alternatively it can be provided directly (with --fingerprint flag)."
@@ -196,7 +204,6 @@ type GlobalOpts struct {
 	DryRun        bool
 	MaxAPIRetries int
 	ConfigFile    string
-	Verbose       bool
 	Debug         bool
 }
 
@@ -235,9 +242,9 @@ func newRootCmd(out io.Writer, args []string) (*cobra.Command, error) {
 	}
 	cmd.PersistentFlags().StringVarP(&global.ApiToken, "api-token", "a", "", apiTokenFlag)
 	cmd.PersistentFlags().StringVar(&global.Org, "org", "", orgFlag)
-	cmd.PersistentFlags().StringVarP(&global.Host, "host", "H", "https://app.kosli.com", hostFlag)
-	cmd.PersistentFlags().IntVarP(&global.MaxAPIRetries, "max-api-retries", "r", maxAPIRetries, maxAPIRetryFlag)
-	cmd.PersistentFlags().StringVarP(&global.ConfigFile, "config-file", "c", defaultConfigFilename, configFileFlag)
+	cmd.PersistentFlags().StringVarP(&global.Host, "host", "H", defaultHost, hostFlag)
+	cmd.PersistentFlags().IntVarP(&global.MaxAPIRetries, "max-api-retries", "r", defaultMaxAPIRetries, maxAPIRetryFlag)
+	cmd.PersistentFlags().StringVarP(&global.ConfigFile, "config-file", "c", defaultConfigFilePath(), configFileFlag)
 	cmd.PersistentFlags().BoolVar(&global.Debug, "debug", false, debugFlag)
 
 	// Add subcommands
@@ -270,6 +277,7 @@ func newRootCmd(out io.Writer, args []string) (*cobra.Command, error) {
 		newDisableCmd(out),
 		newEnableCmd(out),
 		newTagCmd(out),
+		newConfigCmd(out),
 	)
 
 	cobra.AddTemplateFunc("isBeta", isBeta)
@@ -279,12 +287,22 @@ func newRootCmd(out io.Writer, args []string) (*cobra.Command, error) {
 	return cmd, nil
 }
 
+func defaultConfigFilePath() string {
+	if _, ok := os.LookupEnv("DOCS"); ok { // used for docs generation
+		return fmt.Sprintf("$HOME/%s", defaultConfigFilename)
+	}
+	home, err := homedir.Dir()
+	if err == nil {
+		path := filepath.Join(home, defaultConfigFilename)
+		_, err := os.Stat(path)
+		if err == nil {
+			return path
+		}
+	}
+	return "kosli" // for backward compatibility with old default config location
+}
+
 func initialize(cmd *cobra.Command, out io.Writer) error {
-	logger.DebugEnabled = global.Debug
-	logger.SetInfoOut(out) // needed to allow tests to overwrite the logger output stream
-	kosliClient.SetDebug(global.Debug)
-	kosliClient.SetMaxAPIRetries(global.MaxAPIRetries)
-	kosliClient.SetLogger(logger)
 	v := viper.New()
 
 	// If provided, extract the custom config file dir and name
@@ -292,7 +310,7 @@ func initialize(cmd *cobra.Command, out io.Writer) error {
 	// handle passing the config file as an env variable.
 	// we load the config file before we bind env vars to flags,
 	// so we check for the config file env var separately here
-	if global.ConfigFile == defaultConfigFilename {
+	if global.ConfigFile == defaultConfigFilePath() {
 		if path, exists := os.LookupEnv("KOSLI_CONFIG_FILE"); exists {
 			global.ConfigFile = path
 		}
@@ -304,7 +322,7 @@ func initialize(cmd *cobra.Command, out io.Writer) error {
 	v.SetConfigName(file)
 
 	// Set as many paths as you like where viper should look for the
-	// config file. We are only looking in the current working directory.
+	// config file. By default, we are looking in the current working directory.
 	if dir == "" {
 		dir = "."
 	}
@@ -333,6 +351,12 @@ func initialize(cmd *cobra.Command, out io.Writer) error {
 	// Bind the current command's flags to viper
 	bindFlags(cmd, v)
 
+	logger.DebugEnabled = global.Debug
+	logger.SetInfoOut(out) // needed to allow tests to overwrite the logger output stream
+	kosliClient.SetDebug(global.Debug)
+	kosliClient.SetMaxAPIRetries(global.MaxAPIRetries)
+	kosliClient.SetLogger(logger)
+
 	return nil
 }
 
@@ -351,6 +375,20 @@ func bindFlags(cmd *cobra.Command, v *viper.Viper) {
 		// Apply the viper config value to the flag when the flag is not set and viper has a value
 		if !f.Changed && v.IsSet(f.Name) {
 			val := v.Get(f.Name)
+			if f.Name == "api-token" {
+				// get encryption key
+				key, err := security.GetSecretFromCredentialsStore(credentialsStoreKeySecretName)
+				if err != nil {
+					logger.Error("failed to get api token encryption key from credentials store: %s", err)
+				}
+				// decrypt token
+				decryptedBytes, err := security.AESDecrypt([]byte(val.(string)), []byte(key))
+				if err != nil {
+					logger.Error("failed to decrypt api token: %s", err)
+				}
+				val = string(decryptedBytes)
+			}
+
 			if err := cmd.Flags().Set(f.Name, fmt.Sprintf("%v", val)); err != nil {
 				logger.Error("failed to set flag: %v", err)
 			}
