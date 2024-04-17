b54dbb0b0f55a332dcf450663885b1b6dd14781d
Sami Alajrami <sami@kosli.com>
2024-04-17 14:38:00 +0200

    fail if any required flag is passed by user as an empty string

--- cmd/kosli/attestGeneric_test.go
+++ cmd/kosli/attestGeneric_test.go
@@ -66,6 +66,12 @@ func (suite *AttestGenericCommandTestSuite) TestAttestGenericCmd() {
 			golden:    "Error: Artifact with fingerprint '1234e5bda0c762d2bac7f90d758b5b2263fa01ccbc542ab5e3df163be08e6ca9' does not exist in flow 'attest-generic' belonging to organization 'docs-cmd-test-user'\n",
 		},
 		{
+			wantError: true,
+			name:      "fails when --name is passed as empty string",
+			cmd:       fmt.Sprintf("attest generic --name \"\" --commit HEAD --origin-url example.com  %s", suite.defaultKosliArguments),
+			golden:    "Error: flag '--name' is required, but empty string was provided\n",
+		},
+		{
 			name:   "can attest generic against an artifact using artifact name and --artifact-type",
 			cmd:    fmt.Sprintf("attest generic testdata/file1 --artifact-type file --name foo --commit HEAD --origin-url example.com  %s", suite.defaultKosliArguments),
 			golden: "generic attestation 'foo' is reported to trail: test-123\n",
