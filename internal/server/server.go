60def713066e193d15e16739d87f9c18fc76ae3d
Tore Martin Hagen <tore@kosli.com>
2024-04-24 11:59:36 +0200

    Continued work on path spec file

--- internal/server/server.go
+++ internal/server/server.go
@@ -73,6 +73,59 @@ func CreateServerArtifactsData(paths, excludePaths []string, logger *logger.Logg
 	return result, nil
 }
 
+// CreateServerArtifactsData creates a list of ServerData for server artifacts at given paths
+// paths and excludePaths can contain Glob patterns
+// if paths have Glob patterns, each path matching the pattern will be treated as an artifact
+func FingerprintPaths(artifacName string, paths, excludePaths []string, logger *logger.Logger) (*ServerData, error) {
+	result := &ServerData{}
+
+	pathsToInclude := []string{}
+	for _, p := range paths {
+		found, err := filepathx.Glob(p)
+		if err != nil {
+			return result, err
+		}
+		pathsToInclude = append(pathsToInclude, found...)
+	}
+
+	if len(pathsToInclude) == 0 {
+		return result, fmt.Errorf("no matches found for %v", paths)
+	}
+
+	fingerprintAfterEachOther := ""
+	for _, p := range pathsToInclude {
+		// digests := make(map[string]string)
+
+		finfo, err := os.Stat(p)
+		if err != nil {
+			return result, fmt.Errorf("failed to open path %s with error: %v", p, err)
+		}
+		var fingerprint string
+		if !finfo.IsDir() {
+			fingerprint, err = digest.FileSha256(p)
+		} else {
+			fingerprint, err = digest.DirSha256(p, excludePaths, logger)
+		}
+		if err != nil {
+			return result, fmt.Errorf("failed to get a digest of path %s with error: %v", p, err)
+		}
+		fingerprintAfterEachOther += fingerprint
+		fmt.Printf("%s\n", fingerprintAfterEachOther)
+
+		// artifactName, err := filepath.Abs(p)
+		// if err != nil {
+		// 	return result, fmt.Errorf("failed to get absolute path for %s with error: %v", p, err)
+		// }
+		// digests[artifactName] = fingerprint
+		// ts, err := getPathLastModifiedTimestamp(p)
+		// if err != nil {
+		// 	return result, fmt.Errorf("failed to get last modified timestamp of path %s with error: %v", p, err)
+		// }
+		// result = append(result, &ServerData{Digests: digests, CreationTimestamp: ts})
+	}
+	return result, nil
+}
+
 // getPathLastModifiedTimestamp returns the last modified timestamp of a directory
 func getPathLastModifiedTimestamp(path string) (int64, error) {
 	fileInfo, err := os.Stat(path)
@@ -125,6 +178,17 @@ func CreatePathsArtifactsData(pathSpecFile string, logger *logger.Logger) ([]*Se
 	if err := v.UnmarshalExact(&ps); err != nil {
 		return result, fmt.Errorf("failed to unmarshal path spec file [%s] : %v", pathSpecFile, err)
 	}
+
+	for artifactName, paths := range ps.Artifacts {
+		fmt.Printf("%s with %v\n", artifactName, paths)
+		res, err := FingerprintPaths(artifactName, paths.Include, paths.Exclude, logger)
+		if err != nil {
+			return result, err
+		}
+		result = append(result, res)
+	}
+
+	fmt.Println(result[0])
 
 	// pathsToInclude := []string{}
 	// for _, p := range paths {
