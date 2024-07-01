5c694e4c66464dc8f336432548efe124d4121e25
Tore Martin Hagen <93583343+ToreMerkely@users.noreply.github.com>
2024-07-01 10:32:57 +0200

    Updated docker compose to v2 (#249)

--- bin/reset-or-start-server.sh
+++ bin/reset-or-start-server.sh
@@ -17,9 +17,9 @@ restart_server()
 {
 	echo restarting server ...
 	./bin/docker_login_aws.sh staging
-    docker-compose down || true
+    docker compose down || true
 	docker pull 772819027869.dkr.ecr.eu-central-1.amazonaws.com/merkely:latest || true
-	docker-compose up -d
+	docker compose up -d
 	./mongo/ip_wait.sh localhost:9010/minio/health/live
 	./mongo/ip_wait.sh localhost:8001/ready
 	check_success
