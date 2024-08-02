65935a6615d66bf0bf9742f8ea5b017bf9945818
JonJagger <jon@kosli.com>
2024-08-02 08:33:30 +0100

    Fix [--compliant false] silently turning into [--compliant=true]
    Because of cobra's boolean flag behaviour (see https://docs.kosli.com/faq/#boolean-flags)
    This command:
    
    kosli attest generic --name never-alone-control --compliant false
    
    ends up as:
    
    kosli attest generic --name=never-alone-control --compliant=true false
    
    and `false` is taken as the (unused) artifact-name in
    
    `kosli attest generic [IMAGE-NAME | FILE-PATH | DIR-PATH] [flags]`
    
    This fixes this by checking that when an artifact-name is provided
    you must also provide the --artifact-type flag.

--- cmd/kosli/cli_utils.go
+++ cmd/kosli/cli_utils.go
@@ -520,7 +520,13 @@ func ValidateAttestationArtifactArg(args []string, artifactType, inputSha256 str
 	if artifactType != "" && (len(args) == 0 || args[0] == "") {
 		return fmt.Errorf("artifact name argument is required when --artifact-type is set")
 	}
-
+	if artifactType == "" && (len(args) > 0) {
+		extraMessage := ""
+		if args[0] == "true" || args[0] == "false" {
+			extraMessage = "\nSee https://docs.kosli.com//faq/#boolean-flags"
+		}
+		return fmt.Errorf("--artifact-type is required when artifact name ('%s') argument is supplied.%s", args[0], extraMessage)
+	}
 	if inputSha256 != "" {
 		if err := digest.ValidateDigest(inputSha256); err != nil {
 			return err
