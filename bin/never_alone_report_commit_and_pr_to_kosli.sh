d77f0dc4a9aaaf6febff3e7d330122292c2756da
Tore Martin Hagen <93583343+ToreMerkely@users.noreply.github.com>
2024-08-12 09:15:05 +0200

    Never alone: Better error handling (#292)

--- bin/never_alone_report_commit_and_pr_to_kosli.sh
+++ bin/never_alone_report_commit_and_pr_to_kosli.sh
@@ -79,8 +79,10 @@ function get_commit_and_pull_request
     local commit_sha=$1; shift
     local result_file=$1; shift
 
-    pr_data=$(gh pr list --search "${commit_sha}" --state merged --json author,reviews,mergeCommit,mergedAt,reviewDecision,url)
-    commit_data=$(gh search commits --hash "${commit_sha}" --json author)
+    pr_data=$(gh pr list --search "${commit_sha}" --state merged --json author,reviews,mergeCommit,mergedAt,reviewDecision,url) \
+        || die "Failed to get pull request with: gh pr list --search"
+    commit_data=$(gh search commits --hash "${commit_sha}" --json author) \
+        || die "Failed to get commit with: gh search commits"
 
     combined_data=$(jq -n --arg commitsha "$commit_sha" --argjson commit "$commit_data" --argjson pr "$pr_data" \
       '{commit_sha: $commitsha, commit: $commit[0], pull_request: $pr[0]}')
@@ -118,11 +120,7 @@ function get_commit_and_pull_request
     combined_data=$(echo "${combined_data}" | jq ". += {\"compliant\": $compliant}")
     echo "${combined_data}" > ${result_file}
 
-    if [ "${compliant}" == "true" ]; then
-        return 0
-    else
-        return 1
-    fi
+    echo ${compliant}
 }
 
 function get_commit_and_pr_data_and_report_to_kosli
@@ -131,13 +129,14 @@ function get_commit_and_pr_data_and_report_to_kosli
     local proposed_commit=$1; shift
     local commit_pull_request_flow=$1; shift
     local trail_name=$1; shift
+    local commits compliant
 
-    commits=($(gh api repos/:owner/:repo/compare/${base_commit}...${proposed_commit} -q '.commits[].sha'))
+    commits=($(gh api repos/:owner/:repo/compare/${base_commit}...${proposed_commit} -q '.commits[].sha')) \
+        || die "Failed to get list of commits between '${base_commit}' and '${proposed_commit}' with: gh api"
     for commit_sha in "${commits[@]}"; do
-        short_commit_sha=${commit_sha:0:7}
+        local short_commit_sha=${commit_sha:0:7}
         local file_name="commit_pr_${short_commit_sha}.json"
-        local compliant="true"
-        get_commit_and_pull_request ${commit_sha} ${file_name} || compliant="false"
+        compliant=$(get_commit_and_pull_request ${commit_sha} ${file_name})
         kosli attest generic \
             --name=commit_${short_commit_sha} \
             --compliant=${compliant} \
