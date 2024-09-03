efd897e0b458ce8e8ee7a2cd269926b94922b347
Tore Martin Hagen <tore@kosli.com>
2024-09-03 13:38:22 +0200

    get attestation using api

--- bin/never_alone_create_review_trail.sh
+++ bin/never_alone_create_review_trail.sh
@@ -105,17 +105,59 @@ EOF
         --template-file=${trail_template_file_name}
 }
 
+function get_never_alone_attestation_in_trail
+{
+    local commit_flow=$1; shift
+    local trail_name=$1; shift
+    local slot_name="never-alone-data"
+    local -r curl_output_file=$(mktemp)
+
+
+    http_code=$(curl -X 'GET' \
+        --user ${KOSLI_API_TOKEN}:unused \
+        "${KOSLI_HOST}/api/v2/attestations/${KOSLI_ORG}/${commit_flow}/trail/${trail_name}/${slot_name}" \
+        -H 'accept: application/json' \
+        --output "${curl_output_file}" \
+        --write-out "%{http_code}" \
+        --silent)
+
+    if [[ ${http_code} -lt 200 || ${http_code} -gt 299 ]] ; then
+        >&2 cat "${curl_output_file}"
+        rm "${curl_output_file}"
+        echo "[]"
+        return
+    fi
+
+    cat "$curl_output_file"
+    rm "${curl_output_file}"
+}
+
+function get_never_alone_compliance
+{
+
+    echo false
+}
+
 function attest_commit_trail_never_alone
 {
     local release_flow=$1; shift
     local trail_name=$1; shift
     local -r commit=$1; shift
+    # local link_to_attestation=https://app.kosli.com/${{inputs.kosli_org}}/flows/${CODE_REVIEW_FLOW}/trails/${{inputs.trail_name}}
+    local link_to_attestation=http://localhost/${KOSLI_ORG}/flows/cli/trails/${commit}
 
-    kosli attest generic \
-        --flow ${release_flow} \
-        --trail ${trail_name} \
-        --name="sha_${commit}" \
-        --compliant=true
+    never_alone_data=$(get_never_alone_attestation_in_trail cli ${commit})
+    if [ "${never_alone_data}" != "[]" ]; then
+
+        compliant=$(get_never_alone_compliance "${never_alone_data}")
+
+        kosli attest generic \
+            --flow ${release_flow} \
+            --trail ${trail_name} \
+            --name="sha_${commit}" \
+            --compliant=${compliant} \
+            --external-url never-alone-data=${link_to_attestation}
+    fi
 }
 
 function main
@@ -125,6 +167,7 @@ function main
     # local -r base_commit=$($(repo_root)/bin/never_alone_get_commit_of_latest_release.sh)
     # base_commit="ad4500e73dcb6fb980bcc2b12f44f0750a4adfcc"
     base_commit="d9a332df12ec3883f48b0d79858be5ef9c2bed45"
+    base_commit="4d6ccf339e627ea850071e859f93a34b53284512"
 
     # Use gh instead of git so we can keep the commit depth of 1. The order of the response for gh is reversed
     # so I do a tac at the end to get it the same order.
@@ -133,7 +176,9 @@ function main
     begin_trail_with_template ${RELEASE_FLOW} ${TRAIL_NAME} "${commits[@]}"
     
     for commit in "${commits[@]}"; do
+        set +e
         attest_commit_trail_never_alone ${RELEASE_FLOW} ${TRAIL_NAME} $commit
+        set -e
     done
 }
 
