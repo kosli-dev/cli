55b638c7ff71222546909a243dc978578f749579
Tore Martin Hagen <tore@kosli.com>
2024-09-04 08:45:23 +0200

    Some script cleanup

--- bin/never_alone/create_review_trail_for_release.sh
+++ bin/never_alone/create_review_trail_for_release.sh
@@ -95,6 +95,8 @@ function begin_trail_with_template
     local trail_name=$1; shift
     local commits=("$@")
     local trail_template_file_name="review_trail.yaml"
+
+    # Create a template yaml file
     {
     cat <<EOF
 version: 1
@@ -143,13 +145,12 @@ function get_never_alone_attestation_in_trail
 
 function get_never_alone_compliance
 {
-    local never_alone_data=$1; shift
-    local pr_data=$(echo "${never_alone_data}" | jq '.user_data.pullRequest')
-    local compliant="false"
-    reviews=$(echo "${pr_data}" | jq '.reviews')
-        
-    # github_review_decision=$(echo "${pr_data}" | jq '.reviewDecision')
+    local -r never_alone_data=$1; shift
+    local pr_data compliant reviews pr_author reviews_length review state review_author
 
+    pr_data=$(echo "${never_alone_data}" | jq '.user_data.pullRequest')
+    compliant="false"
+    reviews=$(echo "${pr_data}" | jq '.reviews')
     pr_author=$(echo "${pr_data}" | jq '.author.login')
     reviews_length=$(echo "${pr_data}" | jq '.reviews | length')
     for i in $(seq 0 $(( reviews_length - 1 )))
@@ -167,19 +168,17 @@ function get_never_alone_compliance
 
 function attest_commit_trail_never_alone
 {
-    local release_flow=$1; shift
-    local trail_name=$1; shift
+    local -r release_flow=$1; shift
+    local -r trail_name=$1; shift
     local -r commit=$1; shift
     local -r source_flow=$1; shift
+    local link_to_attestation never_alone_data latest_never_alone_data compliant
 
-    local link_to_attestation="${KOSLI_HOST}/${KOSLI_ORG}/flows/${source_flow}/trails/${commit}"
-
+    link_to_attestation="${KOSLI_HOST}/${KOSLI_ORG}/flows/${source_flow}/trails/${commit}"
     never_alone_data=$(get_never_alone_attestation_in_trail ${source_flow} ${commit})
     if [ "${never_alone_data}" != "[]" ]; then
-
         latest_never_alone_data=$(echo "${never_alone_data}" | jq '.[-1]')
         compliant=$(get_never_alone_compliance "${latest_never_alone_data}")
-
         kosli attest generic \
             --flow ${release_flow} \
             --trail ${trail_name} \
@@ -192,22 +191,14 @@ function attest_commit_trail_never_alone
 function main
 {
     check_arguments "$@"
-    # base_commit: the commit of latest release
-    # local -r base_commit=$($(repo_root)/bin/never_alone/get_commit_of_latest_release.sh)
-    # base_commit="ad4500e73dcb6fb980bcc2b12f44f0750a4adfcc"
-    # base_commit="d9a332df12ec3883f48b0d79858be5ef9c2bed45"
-    # base_commit="4d6ccf339e627ea850071e859f93a34b53284512"
-
-    # Use gh instead of git so we can keep the commit depth of 1. The order of the response for gh is reversed
-    # so I do a tac at the end to get it the same order.
-    commits=($(gh api repos/:owner/:repo/compare/${BASE_COMMIT}...${PROPOSED_COMMIT} -q '.commits[].sha' | tac))
+    # Use gh instead of git so we can keep the commit depth of 1. The order are from oldest
+    # commit to newest
+    commits=($(gh api repos/:owner/:repo/compare/${BASE_COMMIT}...${PROPOSED_COMMIT} -q '.commits[].sha'))
 
     begin_trail_with_template ${RELEASE_FLOW} ${TRAIL_NAME} "${commits[@]}"
     
     for commit in "${commits[@]}"; do
-        set +e
         attest_commit_trail_never_alone ${RELEASE_FLOW} ${TRAIL_NAME} ${commit} ${SOURCE_FLOW}
-        set -e
     done
 }
 
