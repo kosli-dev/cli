ee637050fad7060c985f27c2be4a59022d986863
JonJagger <jon@kosli.com>
2025-03-07 04:17:48 +0000

    Omit needless words

--- cmd/kosli/snapshotPath.go
+++ cmd/kosli/snapshotPath.go
@@ -20,7 +20,7 @@ const snapshotPathShortDesc = `Report a snapshot of a single artifact running in
 const snapshotPathLongDesc = snapshotPathShortDesc + `
 You can report a directory or file artifact. For reporting multiple artifacts in one go, use "kosli snapshot paths".
 You can exclude certain paths or patterns from the artifact fingerprint using ^--exclude^.
-The supported glob pattern syntax is what is documented here: https://pkg.go.dev/path/filepath#Match , 
+The supported glob pattern syntax is documented here: https://pkg.go.dev/path/filepath#Match ,
 plus the ability to use recursive globs "**"
 
 ` + kosliIgnoreDesc
