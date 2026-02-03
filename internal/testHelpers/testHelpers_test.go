152d5931f4f544d6320b5ca34a3a4357009d5286
Tore Martin Hagen <93583343+ToreMerkely@users.noreply.github.com>
2026-02-03 08:40:48 +0100

    Added minimum _test.go files for files that does not have tests. (#650)
    We use the tool cover for coverage, but if there is no _test.go file,
    then Go use the tool covdata. At the same time, they removed covdata
    as a precompiled binary in the distribution. This made the coverage
    calculation fail for some of us.
    
    As described in this discussion
      https://github.com/golang/go/issues/75031
    there is an other option to fix this by modifying the
    GOTOOLCHAIN variable, but that is too much magic for me and
    could be very hard to debug for the next person who stumbles
    over this.

---
+++ internal/testHelpers/testHelpers_test.go
@@ -0,0 +1,9 @@
+package testHelpers_test
+
+import "testing"
+
+func TestPlaceholder(t *testing.T) {
+	// We use the tool cover for coverage, but if there is no _test.go file, then
+	// Go use the tool covdata. At the same time, they removed covdata as a precompiled
+	// binary in the distribution. This made the coverage calculation fail for some of us.
+}
