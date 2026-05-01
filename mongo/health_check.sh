678ef5485cf97592c9f8834c5aae5f8d9b9aa846
Marko Bevc <marko@kosli.com>
2026-05-01 22:00:48 +0100

    chore: use supported mongosh

--- mongo/health_check.sh
+++ mongo/health_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -Eeu
 
 OK=$(echo "rs.status().ok" | \
-  mongo --username ${MONGO_INITDB_ROOT_USERNAME} \
+  mongosh --username ${MONGO_INITDB_ROOT_USERNAME} \
         --password ${MONGO_INITDB_ROOT_PASSWORD} \
         --quiet)
 
