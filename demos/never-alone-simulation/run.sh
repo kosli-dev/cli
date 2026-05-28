5fb0cbc24c98906d6a589cdad66b2fa34b6de72c
Sofus Albertsen <sofusalbertsen@users.noreply.github.com>
2026-05-28 21:18:47 +0200

    Adding trail data from kosli cli as well as rego to the demos folder

---
+++ demos/never-alone-simulation/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#../../kosli evaluate input --policy four-eyes.rego -i trails/v2.11.42-pass.json --decision
+../../kosli evaluate input --policy four-eyes.rego -i trails/v2.11.44-fail.json --decision
