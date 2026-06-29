0ce7eed8f2e87e62b3a135fa6aef4d404bc467ac
Marko Bevc <marko@kosli.com>
2026-06-29 05:56:29 +0100

    feat(update): add default-org cmd to set user default organization (#977)
    * feat(update): add default-org subcommand to set user default organization
    
    Adds `kosli update default-org ORG-NAME` which calls PUT /api/v2/user/{org}
    to set the default organization for the currently authenticated user.
    
    * fix: update default-org testing
    
    * chore: add longDesc for update defaulr-rg
    
    * chore: check for empty org-name and add Dry run testing

---
+++ cmd/kosli/update.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+
+	"github.com/spf13/cobra"
+)
+
+const updateDesc = `All Kosli update commands.`
+
+func newUpdateCmd(out io.Writer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "update",
+		Aliases: []string{"u", "up"},
+		Short:   updateDesc,
+		Long:    updateDesc,
+	}
+
+	// Add subcommands
+	cmd.AddCommand(
+		newUpdateDefaultOrgCmd(out),
+	)
+
+	return cmd
+}
